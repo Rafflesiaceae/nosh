7f175fa2bae58b55a851bc6d523358b522e8207c
Rafflesiaceae <rafflesiaceae.plant@gmail.com>
2022-05-21 13:13:56 +0200

    Support multiple args for fs.chdir

--- fs/chdir.go
+++ fs/chdir.go
@@ -3,33 +3,31 @@ package fs
 import (
 	"os"
 
+	"github.com/Rafflesiaceae/nosh/internal"
 	"go.starlark.net/starlark"
 )
 
 func chdir(thread *starlark.Thread, fn *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
 	var err error
 
-	var (
-		path string
-	)
-
-	if err = starlark.UnpackArgs("chdir", args, kwargs, "path", &path); err != nil {
-		return nil, err
+	var paths []string
+	if paths, err = internal.UnpackPositionalVarargsString("chdir", args); err != nil {
+		return starlark.Bool(false), err
 	}
 
-	var pwd string
-	if pwd, err = os.Getwd(); err != nil {
-		// PWD probably doesn't exist (anymore?)
-		pwd = ""
-	}
-
-	if err := os.Chdir(path); err != nil {
-		return nil, err
+	pwd, _ := os.Getwd()
+	for _, path := range paths {
+		if err = os.Chdir(path); err != nil {
+			return nil, err
+		}
 	}
 
 	if pwd != "" && pwd != peekDirStack().GoString() {
+		// only push previous working dir to dirStack if it is not the same as
+		// the last path stored in dirStack
+
 		dirStack.Append(starlark.String(pwd))
 	}
 
-	return starlark.String(path), nil
+	return starlark.String(pwd), nil
 }
